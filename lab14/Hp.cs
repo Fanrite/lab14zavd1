685af5c224a36493320df318e3ce9baae09f8c98
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-03-15 23:22:35 +0200

    Добавьте файлы проекта.

---
+++ lab14/Hp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hlp
+{
+    class Hp
+    {
+        public static void Gen (double[,] arr, int n, int m, double a, double b)
+        {
+            Random ran = new Random();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    arr[i, j] = a + ran.NextDouble() * (b - a);
+        }
+
+        public static void Printer(double[,] arr, int n, int m, ref string output)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                output += "|";
+                for (int j = 0; j < m; j++)
+                    output += $"{arr[i, j]}|";
+                output += "\n\n";
+            }
+        }
+
+        public static void Maxpos (double[,] arr, int n, int m, ref string rezult)
+        {
+            double buf = 0;
+            int indxi = 0, indxj = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    if (arr[i,j] > buf)
+                    {
+                        buf = arr[i, j];
+                        indxi = i;
+                        indxj = j;
+                    }
+
+            rezult += $"Максимальний: arr[{indxi},{indxj}] = {buf}";
+        }
+    }
+}
---
+++ lab14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hlp;
+
+namespace lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 300;
+            int n = 1, m = 1;
+            bool flag = true;
+            while (flag)
+            {
+                string output = "\n", vih, rezult = "";
+                Console.Write("Введіть кількість рядків:");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введіть кількість стоібців:");
+                m = Convert.ToInt32(Console.ReadLine());
+
+                double[,] arr = new double[n, m];
+                Hp.Gen(arr, n, m, a, b);
+                Hp.Printer(arr, n, m, ref output);
+                Hp.Maxpos(arr, n, m, ref rezult);
+
+                Console.WriteLine(output);
+                Console.WriteLine(rezult);
+
+                Console.Write("Бажаєте повторити?(Y/N):");
+                vih = Console.ReadLine();
+                if (vih != "y" && vih != "Y") flag = false;
+            }
+
+        }
+    }
+}
